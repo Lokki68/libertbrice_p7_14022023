9dac0255e1ab24d186267ca9a6a533a738c24c1b
Brice <bricel67@gmail.com>
2023-02-17 13:36:06 +0100

    add login and register user

---
+++ frontEnd/tailwind.config.cjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'display': ['Inconsolata', 'sans-serif'],
+      'body': ['Roboto', 'sans-serif'],
+    },
+    backdropFilter: {
+      'none': 'none',
+      'blur': 'blur(10px)',
+    },
+
+    extend: {
+      backGroundImage: {
+        'main-pattern': "url('/assets/photo_entreprise.jpg')"
+      },
+      colors: {
+        'primary': {
+          100: '#fcd4d3',
+          200: '#fc947c',
+          300: '#fc2f08'
+        },
+        'black': '#040404',
+        'white': '#f1f1f1',
+        'fond': '#e5e5e5',
+        'success': '#35591a',
+        'alert': '#591a1a'
+      },
+      gridTemplateColumns: {
+        'comment': '2fr 4fr 1fr'
+      }
+    },
+  },
+  plugins: [],
+}
